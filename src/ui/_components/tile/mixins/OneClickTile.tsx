73c3e3dcec06c094efb83cf5f01a9ec098e4d28a
Dergunov Egor <edergunov@addison.de>
2017-04-27 12:28:18 +0200

    (ノﾟ∀ﾟ)ノ⌒･*:.｡. .｡.:*･゜ﾟ･*☆

--- src/ui/_components/tile/mixins/OneClickTile.tsx
+++ src/ui/_components/tile/mixins/OneClickTile.tsx
@@ -2,9 +2,16 @@ import * as React from "react";
 import TileProps from "./../TileProps";
 
 class OneClickTile extends React.Component<TileProps, null> {
+    private openLink(url: string) {
+        window.open(url, '_blank').focus();
+    }
+
     public render() {
         return (
-            <div></div>
+            <div style={ { padding: "20px" } } onClick={ this.openLink.bind(this, this.props.content.directLink) }>
+                <div style={ { color: "#0081c6" } }>ADDISON</div>
+                <div style={ { color: "#E6742E" } }>OneClick</div>
+            </div>
         );
     }
 }
